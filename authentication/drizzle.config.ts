93ec483a5c066d2f1f3698f62d8ee905ef878eab
rayhan randi <rayhanrandi10@gmail.com>
2025-05-24 00:20:35 +0700

    hotfix: fix loki endpoint to svcname

--- authentication/drizzle.config.ts
+++ authentication/drizzle.config.ts
@@ -11,6 +11,6 @@ export default {
     user: process.env.DB_USER ?? "postgres",
     password: process.env.DB_PASSWORD ?? "postgres",
     database: process.env.DB_NAME ?? "postgres",
-    ssl: false,
+    ssl: true
   },
 } satisfies Config;
